885ddc196ee2e4d0b5e75bb01580c00fb848a9d6
Marcel <firebird10@gmx.de>
2022-11-06 16:47:07 +0100

    feat: stdio/out for repl mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::Write;
+use std::{env, io};
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    match args.len() {
+        0 => run_prompt().unwrap_or_else(|err| eprintln!("error: {}", err)),
+        1 => run_file(&args[0]).unwrap_or_else(|err| eprintln!("error: {}", err)),
+        _ => eprintln!("Usage: lox [script]"),
+    };
+
+    Ok(())
 }
+
+fn run_file(arg: &String) -> io::Result<()> {
+    Ok(())
+}
+fn run_prompt() -> io::Result<()> {
+    loop {
+        let mut out = io::stdout().lock();
+        out.write_all(b"> ")?;
+        out.flush()?;
+
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => match n {
+                0 => break,
+                _ => {
+                    // dbg!("{}", input);
+                    print!("{input}");
+                    run();
+                }
+            },
+            Err(err) => print!("{err}"),
+        }
+    }
+    Ok(())
+}
+
+fn run() {}
