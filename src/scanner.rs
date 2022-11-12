ba6888c5c27dc7f232cc17f971d31c9179a2f92d
Marcel <firebird10@gmx.de>
2022-11-12 12:51:09 +0100

    feat(scanner): use chars instead of string

--- src/scanner.rs
+++ src/scanner.rs
@@ -18,8 +18,8 @@ impl Scanner<'_> {
     pub fn scan_tokens(&mut self) -> Vec<Token> {
         // split source string into tokens
 
-        let c = self.source.split("");
-        for i in c {
+        let chars_iter = self.source.chars();
+        for i in chars_iter {
             self.scan_token(i);
         }
 
@@ -30,18 +30,18 @@ impl Scanner<'_> {
 
     fn add_token(&self, token_type: TokenType, literal: &str) {}
 
-    fn scan_token<'a>(&self, c: &str) {
+    fn scan_token<'a>(&self, c: char) {
         match c {
-            "(" => self.add_token(TokenType::LeftParen, "test"),
-            ")" => self.add_token(TokenType::RightParen, "test"),
-            "{" => self.add_token(TokenType::LeftBrace, "test"),
-            "}" => self.add_token(TokenType::RightBrace, "test"),
-            "," => self.add_token(TokenType::Comma, "test"),
-            "." => self.add_token(TokenType::Dot, "test"),
-            "-" => self.add_token(TokenType::Minus, "test"),
-            "+" => self.add_token(TokenType::Plus, "test"),
-            ";" => self.add_token(TokenType::Semicolon, "test"),
-            "*" => self.add_token(TokenType::Star, "test"),
+            '(' => self.add_token(TokenType::LeftParen, "test"),
+            ')' => self.add_token(TokenType::RightParen, "test"),
+            '{' => self.add_token(TokenType::LeftBrace, "test"),
+            '}' => self.add_token(TokenType::RightBrace, "test"),
+            ',' => self.add_token(TokenType::Comma, "test"),
+            '.' => self.add_token(TokenType::Dot, "test"),
+            '-' => self.add_token(TokenType::Minus, "test"),
+            '+' => self.add_token(TokenType::Plus, "test"),
+            ';' => self.add_token(TokenType::Semicolon, "test"),
+            '*' => self.add_token(TokenType::Star, "test"),
             _ => eprintln!(
                 "{}",
                 LoxError::SyntaxError {
